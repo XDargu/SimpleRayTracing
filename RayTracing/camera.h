ed26b9879732684fccd33be3d44dcd6756d853ad
XDargu <xdargu@gmail.com>
2025-04-09 01:37:43 +0200

    Use Lambertian Reflections

--- RayTracing/camera.h
+++ RayTracing/camera.h
@@ -103,7 +103,7 @@ private:
         HitRecord rec;
         if (world.Hit(r, Interval(0.001, infinity), rec))
         {
-            const Vec3 direction = Random::OnHemisphere(rec.normal);
+            const Vec3 direction = rec.normal + Random::UnitVector();
             return 0.5 * RayColor(Ray(rec.p, direction), depth - 1, world);
         }
 
